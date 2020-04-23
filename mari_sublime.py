f85972ff68b1e9033eaa0a8282a575db665f8480
cg-cnu <sreenivas9alapati@gmail.com>
2020-04-23 14:31:42 +0930

    refactor

--- mari_sublime.py
+++ mari_sublime.py
@@ -1,59 +1,91 @@
 #!/usr/bin/python
 import os
+import re
 import sys
 import sublime
 import sublime_plugin
 from telnetlib import Telnet
 
-HOST = 'localhost'
-PORT = 6100
 
-def append_lib_in_sys_paths():
-    """add library in sys path
+def message(message):
+    """ Display message to the user
     """
-    lib_dir = os.path.abspath(os.path.join(__file__, '..', 'lib'))
+    return sublime.error_message("mariSublime: {}".format(message))
 
-    if lib_dir not in sys.path:
-        sys.path.append(lib_dir)
 
-class MariCommand(sublime_plugin.TextCommand):
-    """send a sublime remote command 
+class MariRemote:
+    """ Mari Remote connection
     """
+
+    def __init__(self, host="127.0.0.1", port=7002):
+        """construct
+        Args:
+            host (str): name of the host
+            remote (int: port number
+        """
+        self.host = host
+        self.port = port
+
+    def __call__(self, code):
+        """evaluate given python code
+
+        Args:
+            code (str): python code string
+        """
+        try:
+            connection = Telnet(self.host, self.port)
+        except ConnectionRefusedError:
+            message(
+                "Connection refused on host {} at port {}.".format(self.host, self.port)
+            )
+            return
+
+        if sys.version_info[0] < 3:
+            connection.write(code)
+            connection.write("\x04")
+        else:
+            connection.write(code.encode(encoding="UTF-8"))
+            connection.write("\x04".encode(encoding="UTF-8"))
+
+        connection.close()
+
+
+class send_to_mariCommand(sublime_plugin.TextCommand):
+    """send to Mari
+    """
+
     def run(self, edit):
         """run command
         """
-        append_lib_in_sys_paths()
+        syntax = self.view.settings().get("syntax")
+        if not re.search(r"python", syntax, re.I):
+            message("Not a valid Language")
+            return
 
-        import mari_remote
-        remote = mari_remote.MariRemote(HOST, PORT)
-        
-        selections = self.view.sel()
-        snips = []
-        selSize = 0
-        
-        for sel in selections:
-            if not sel.empty():
-                selSize += 1
-        
-        if selSize == 0:
-            
-            filePath = self.view.file_name()
-            if filePath is None:
-                sublime.error_message("Save file")
-                return
+        file_path = self.view.file_name()
+        if file_path is None:
+            message("Please save the file")
+            return
 
-            if self.view.is_dirty():
-                sublime.error_message("Save changes")
-                return
-            
+        if self.view.is_dirty():
+            message("Please save the changes")
+            return
+
+        snippets = []
+        selection_size = 0
+
+        for selection in self.view.sel():
+            if not selection.empty():
+                selection_size += 1
+                snippets.extend(self.view.substr(selection).splitlines())
+
+        if selection_size == 0:
             content = self.view.substr(sublime.Region(0, self.view.size()))
-            snips.extend(content.splitlines())
-        
-        else:
-            for sel in selections:
-                snips.extend( self.view.substr(sel).splitlines() )
-        
-        cmd = str('\n'.join(snips))
-        
-        remote.eval(cmd)
-        
+            snippets.extend(content.splitlines())
+
+        cmd = str("/n".join(snippets))
+
+        settings = sublime.load_settings("mariSublime.sublime-settings")
+        remote = MariRemote(settings.get("host"), settings.get("port"))
+
+        remote(cmd)
