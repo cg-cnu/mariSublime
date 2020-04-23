f0927a6efd983899a1b9f683e7540ccc29c984de
cg-cnu <sreenivas9alapati@gmail.com>
2020-04-23 14:31:11 +0930

    removed examples and lib

--- examples/00_mari_run_script.py
+++
@@ -1,4 +0,0 @@
-from mari_remote import MariRemote
-
-remote = MariRemote('localhost', 6100)
-remote.execfile('01_hello_world.py')
--- examples/01_mari_eval_python.py
+++
@@ -1,5 +0,0 @@
-"""example snippet to eval python code
-"""
-from mari_remote import MariRemote
-remote = MariRemote('localhost', 6100)
-remote.eval('print "adsf"')
